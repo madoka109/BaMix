c3ffc8236326d52bde2b630c9d976c29da22f908
ZhiHua <79368885+madoka109@users.noreply.github.com>
2023-03-23 14:49:10 +0800

    Delete run.sh

--- main/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python cifar_train.py --mixup 1 --devices 0 --dataset cifar10 --imb_factor 0.005 --epochs 400 --drw_epoch 320 --loss_type STM --train_rule DRW --mixepoch 400
-
-python cifar_train.py --mixup 1 --devices 0 --dataset cifar10 --imb_factor 0.1 --epochs 400 --drw_epoch 320 --loss_type STM --train_rule DRW --mixepoch 400
-
-python cifar_train.py --mixup 1 --devices 0 --dataset cifar100 --imb_factor 0.005 --epochs 400 --drw_epoch 320 --loss_type STM --train_rule DRW --mixepoch 400
-
-python cifar_train.py --mixup 1 --devices 0 --dataset cifar100 --imb_factor 0.1 --epochs 400 --drw_epoch 320 --loss_type STM --train_rule DRW --mixepoch 400
